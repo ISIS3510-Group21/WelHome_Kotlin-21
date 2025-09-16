9c494cd4e2fb53bed44d173bb952e90655c475ba
Juan Pablo Baldion <j.baldion@uniandes.edu.co>
2025-09-15 23:32:28 -0500

    Structure directories MVVM pattern

--- app/src/main/java/com/team21/myapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/team21/myapplication/ui/main/MainActivity.kt
@@ -1,12 +1,8 @@
-package com.team21.myapplication
+package com.team21.myapplication.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
