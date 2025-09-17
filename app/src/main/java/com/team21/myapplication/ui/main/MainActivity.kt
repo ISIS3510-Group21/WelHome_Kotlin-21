b31fe9b2bc6d7da9404ddc4680da584edc659ffa
EricAlarconDolynko <e.alarcond@uniandes.edu.co>
2025-09-17 08:55:37 -0500

    Restore MainActivity

---
+++ app/src/main/java/com/team21/myapplication/ui/main/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.team21.myapplication.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.team21.myapplication.ui.theme.WelHomeTheme
+import android.util.Log
+import com.google.firebase.firestore.firestore
+import com.google.firebase.Firebase
+
+class MainActivity : ComponentActivity() {
+
+    val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        probarConexionFirestore()
+    }
+
+    private fun probarConexionFirestore(){
+        val datosDePrueba = hashMapOf(
+            "name" to "Kotlin user"
+        )
+
+        db.collection("StudentUser")
+            .add(datosDePrueba)
+            .addOnSuccessListener { documentReference ->
+                val idDocumento = documentReference.id
+                Log.d("FirestoreTest", "Documento añadido con ID: $idDocumento")
+
+                leerDatosDePrueba(idDocumento)
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Firestore Test", "Errro al añadir al documento", exception)
+            }
+
+    }
+
+    private fun leerDatosDePrueba(idDocumento: String) {
+        db.collection("StudentUser").document(idDocumento)
+            .get()
+            .addOnSuccessListener { document ->
+                // Si la lectura es exitosa, se ejecuta este bloque
+                if (document != null && document.exists()) {
+                    Log.d("FirestoreTest", "✅ Datos leídos correctamente: ${document.data}")
+                } else {
+                    Log.d("FirestoreTest", "⚠️ No se encontró el documento.")
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Si la lectura falla, se ejecuta este bloque
+                Log.w("FirestoreTest", "❌ Error al leer el documento.", exception)
+            }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WelHomeTheme {
+        Greeting("Android")
+    }
+}
