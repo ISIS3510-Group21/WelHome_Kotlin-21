c3a86794b268ba0e1d85b387243afac5f6de6072
MariaAmor8 <m.amorocho@uniandes.edu.com>
2025-10-04 00:30:50 -0500

    Analytics features: home loading time and housing post click

--- app/src/main/java/com/team21/myapplication/data/model/StudentUser.kt
+++ app/src/main/java/com/team21/myapplication/data/model/StudentUser.kt
@@ -1,20 +1,21 @@
 package com.team21.myapplication.data.model
 
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.Timestamp
 
 data class StudentUser(
-    val id: String,
-    val name: String,
-    val email: String,
-    val phoneNumber: String,
-    val photoPath: String,
-    val gender: String,
-    val password: String,
-    val nationality: String,
-    val language: String,
-    val birthDate: String,
-    val university: String,
-    val roomieTags: List<RoomieTag>,
-    val savedBookings: List<DocumentReference>,
-    val savedHousing: List<DocumentReference>
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val phoneNumber: String = "",
+    val photoPath: String = "",
+    val gender: String = "",
+    val password: String= "",
+    val nationality: String= "",
+    val language: String= "",
+    val birthDate: Timestamp = Timestamp.now(),
+    val university: String= "",
+    val roomieTags: List<RoomieTag> = emptyList(),
+    val savedBookings: List<DocumentReference> = emptyList(),
+    val savedHousing: List<DocumentReference> = emptyList()
 )
