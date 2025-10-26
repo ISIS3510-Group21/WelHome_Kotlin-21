a264be7ba412fecf201ce4b9db1a8e9ece41cebf
Juan Pablo Baldion <j.baldion@uniandes.edu.co>
2025-10-25 23:13:36 -0500

    ThreadForum model

--- app/src/main/java/com/team21/myapplication/data/model/ThreadForum.kt
+++ app/src/main/java/com/team21/myapplication/data/model/ThreadForum.kt
@@ -1,4 +1,21 @@
 package com.team21.myapplication.data.model
 
-class ThreadForum {
-}
+import com.google.firebase.Timestamp
+
+data class ThreadForum (
+    val id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val creationDate: Timestamp ?= null,
+    val commentQuantity: Int = 0,
+    val photoPath: String = ""
+)
+
+data class ForumPost (
+    val id: String = "",
+    val content: String = "",
+    val positiveVotes: Int = 0q,
+    val negativeVotes: Int = 0,
+    val creationDate: Timestamp ?= null,
+    val user: String = ""
+)
