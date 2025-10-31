b1b668a1a76f3b5e96dc107810a62496b587e329
Juan Pablo Baldion <j.baldion@uniandes.edu.co>
2025-10-31 00:14:43 -0500

    Refactor MapCacheEntity

--- app/src/main/java/com/team21/myapplication/data/local/dao/MapCacheDao.kt
+++ app/src/main/java/com/team21/myapplication/data/local/dao/MapCacheDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.team21.myapplication.data.model.MapCacheEntry
+import com.team21.myapplication.data.local.entity.MapCacheEntry
 
 @Dao
 interface MapCacheDao {
