19c1e1ef4d12ac20f92150d95ca376f750ff98e5
Juan Pablo Baldion <j.baldion@uniandes.edu.co>
2025-11-02 20:33:00 -0500

    Saved from recently viewed posts

--- app/src/main/java/com/team21/myapplication/data/repository/StudentUserProfileRepository.kt
+++ app/src/main/java/com/team21/myapplication/data/repository/StudentUserProfileRepository.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.tasks.await
 class StudentUserProfileRepository {
     private val db = FirebaseFirestore.getInstance()
     private val studentUserCollection = db.collection("StudentUserProfile")
+    private val housingPostRepository = HousingPostRepository()
     //Se usa un id provisional para probar
     //private val auth = "tcGfBwE5JbhMFbrnfAzHyXolT0t1"
     //private val auth = FirebaseAuth.getInstance().currentUser?.uid
@@ -40,5 +41,31 @@ class StudentUserProfileRepository {
             visitedHousingPosts = visitedHousingPosts ?: emptyList(),
             recommendedHousingPosts = recommendedHousingPosts ?: emptyList()
         )
+    }
+
+    suspend fun addRecentlyViewed(userId: String, housingId: String) {
+        val housingPost = housingPostRepository.getHousingPostById(housingId)
+        if (housingPost != null) {
+            val housingPreview = HousingPreview(
+                id = housingPost.post.id,
+                title = housingPost.post.title,
+                price = housingPost.post.price,
+                rating = housingPost.post.rating.toFloat(),
+                photoPath = housingPost.post.thumbnail,
+                housing = housingPost.post.address
+            )
+
+            studentUserCollection
+                .whereEqualTo("userId", userId)
+                .get()
+                .await()
+                .documents
+                .firstOrNull()
+                ?.reference
+                ?.collection("VisitedHousingPosts")
+                ?.document(housingId)
+                ?.set(housingPreview)
+                ?.await()
+        }
     }
 }
